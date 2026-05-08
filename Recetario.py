ac972a0aba804898b717c8feea3935327f28b0f7
Sisar217 <cesarsolorzano217@gmail.com>
2026-05-08 11:00:39 -0600

    Realicé ajustes menores en las funciones para que la consola separe los textos para que tengan mejor presentación. Cambié el orden de la llamada de las funciones en el menú 1 porque solicitaba datos en el orden equivocado y mandaba error.

--- Recetario.py
+++ Recetario.py
@@ -3,10 +3,11 @@ from os import system
 
 #Funciones para el recetario
 def solicitar_categoria():
-    categoria = input("Ingresa el nombre de la categoría que deseas explorar: ")
+    categoria = input("\nIngresa el nombre de la categoría que deseas explorar: ")
     return categoria
 
 def mostrar_categorias():
+    print("Estas son las categorías disponibles:")
     for categoria in guia.iterdir():
         if categoria.is_dir():
             print(categoria.name)
@@ -21,40 +22,48 @@ def mostrar_recetas(categoria):
         print(receta.stem)
 
 def solicitar_receta():
-    receta = input("Ingresa la receta que deseas buscar: ")
+    receta = input("\nIngresa la receta que deseas buscar: ")
     return receta
 
-def buscar_recetas():
+def buscar_recetas(categoria, receta):
+    ruta = Path(Path.home() / "Recetario" / categoria / receta)
+    return ruta
 
-    return
+def leer_receta(receta):
+    ruta = Path.home() / "Recetario" / receta_elegida
+    contenido = ruta.read_text(encoding='utf-8')
+    print(contenido)
+
 
 nombre_usuario: str = input("Ingresa tu nombre: ")
+system("clear")
 print(f"Hola, {nombre_usuario}. Bienvenido al Recetario.")
 base = Path("Recetario")
 guia = Path(Path.home() / "Recetario")
 cantidad = sum(1 for _ in Path(guia).glob("**/*.txt"))
-print(f"Puedes encontrar los archivos del recetario en {base}.\nHay un total de {cantidad} recetas.")
+print(f"Puedes encontrar los archivos del recetario en la carpeta {base}.\nHay un total de {cantidad} recetas.\n")
 #El código anterior sirve únicamente como presentación o parte inicial del programa.
 
 editando = True
 while editando:
 
-   system("clear")
    menu = int(input("Selecciona una opción del siguiente menú:\n1. Leer recetas\n2. Crear receta\n"
-                 "3. Crear nueva categoría\n4. Eliminar receta\n5. Eliminar categoría\n6. Salir del programa"))
+                 "3. Crear nueva categoría\n4. Eliminar receta\n5. Eliminar categoría\n6. Salir del programa\n"
+                    "\nSelección: "))
    if menu == 1:
        #función que muestra las recetas.
-       categoria_elegida = solicitar_categoria()
-       receta_elegida = solicitar_receta()
+       system("clear")
        mostrar_categorias()
-       solicitar_categoria()
+       categoria_elegida = solicitar_categoria()
        buscar_categoria(categoria_elegida)
        mostrar_recetas(categoria_elegida)
-       buscar_recetas()
-       continuar = input("Desea realizar alguna otra acción?\n1. Si / 2. No")
+       receta_elegida = solicitar_receta()
+       buscar_recetas(categoria_elegida, receta_elegida)
+       leer_receta(receta_elegida)
+       continuar = input("\nDesea realizar alguna otra acción?\n1. Si / 2. No\n\nSelección: ")
        if continuar == 1:
            print("Muy bien. Reiniciaremos el programa. Espere un momentito.")
-       elif continuar != 1:
+       elif continuar == 2:
            print("De acuerdo. Hasta la próxima!")
            editando = False
        else:
